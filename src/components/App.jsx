f87beb24897246000671f75e95f15c4a8d300959
Vitaliy2773 <vitalii64773@gmail.com>
2023-10-09 14:37:25 -1100

    task 2,3,4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import Profile from "./componentUserTask1/Profile";
 import Statistics from "./componentsStatisticsTask2/Statistics";
+import FriendList from "./componentsFriendListTask3/FriendList";
+import Transaction from "./componentsTransactionHistoryTask4/Transactions";
 import user from "./componentUserTask1/user.json";
 import dataStatistics from "./componentsStatisticsTask2/data.json"
+import friendsJson from "./componentsFriendListTask3/friends.json"
+import transactions from "./componentsTransactionHistoryTask4/transactions.json"
 
 export const App = () => {
   return (
@@ -23,9 +27,15 @@ export const App = () => {
         stats={user.stats} />
       
       <Statistics
-        title={dataStatistics.title}
-        // stats={dataStatistics.stats}
+        title="Upload stats"
+        stats={dataStatistics}
       />
+      <FriendList
+        friends={friendsJson}
+      />
+      <Transaction
+      
+      items={transactions}/>
     </div>
   );
 };
--- src/components/componentsStatisticsTask2/Statistics.js
+++ src/components/componentsStatisticsTask2/Statistics.js
@@ -1,34 +1,17 @@
-// [
-//   { "id": "id-1", "label": ".docx", "percentage": 22 },
-//   { "id": "id-2", "label": ".pdf", "percentage": 4 },
-//   { "id": "id-3", "label": ".mp3", "percentage": 17 },
-//   { "id": "id-4", "label": ".psd", "percentage": 47 },
-//   { "id": "id-5", "label": ".pdf", "percentage": 10 }
-// ]
+
 import PropTypes from 'prop-types';
 
-export default function Statistics({title, stats}) {
+export default function Statistics({title,stats}) {
     return (
-        <section class="statistics">
-            {title && <h2 class="title">{title}</h2>}
-
-            <ul class="stat-list">
-                <li class="item">
-                    <span class="label">{stats.label}</span>
-                    <span class="percentage">{stats.percentage}%</span>
+        <section className="statistics">
+            {title && <h2 className="title">{title}</h2>}
+            <ul className="stat-list">
+                {stats.map(stat => (
+                      <li key={stat.id} className="item">
+                    <span className="label">{stat.label}</span>
+                    <span className="percentage">{stat.percentage}%</span>
                 </li>
-                <li class="item">
-                    <span class="label">{stats.label}</span>
-                    <span class="percentage">{stats.percentage}%</span>
-                </li>
-                <li class="item">
-                    <span class="label">{stats.label}</span>
-                    <span class="percentage">{stats.percentage}%</span>
-                </li>
-                <li class="item">
-                    <span class="label">{stats.label}</span>
-                    <span class="percentage">{stats.percentage}%</span>
-                </li>
+                ))}    
             </ul>
         </section>
     );

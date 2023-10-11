f1d27a104e4cd0eae26c9ee0785d26264a5f19e1
Vitaliy2773 <vitalii64773@gmail.com>
2023-10-11 00:24:51 -1100

    add css.modules

--- src/components/componentUserTask1/Profile.js
+++ src/components/componentUserTask1/Profile.js
@@ -1,31 +1,32 @@
 import PropTypes from 'prop-types';
+import css from './Profile.module.css'
 
 export default function Profile({userName,tag,location,userPhotoUrl,stats,items}) {
     return (
-        <div className="profile">
-  <div className="description">
+        <div className={css.profile}>
+  <div className={css.description}>
     <img
       src={userPhotoUrl}
       alt={userName}
-      className="avatar"
+      className={css.avatar}
     />
                 <p className="name">{userName}</p>
                 <p className="tag">{tag}</p>
                 <p className="location">{location}</p>
   </div>
 
-  <ul className="stats">
+  <ul className={css.stats}>
     <li>
-      <span className="label">Followers</span>
-                    <span className="quantity">{stats.followers}</span>
+      <span className={css.label}>Followers</span>
+                    <span className={css.quantity}>{stats.followers}</span>
     </li>
     <li>
-      <span className="label">Views</span>
-                    <span className="quantity">{stats.views}</span>
+      <span className={css.label}>Views</span>
+                    <span className={css.quantity}>{stats.views}</span>
     </li>
     <li>
-      <span className="label">Likes</span>
-                    <span className="quantity">{stats.likes}</span>
+      <span className={css.label}>Likes</span>
+                    <span className={css.quantity}>{stats.likes}</span>
     </li>
   </ul>
 </div>
